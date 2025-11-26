3a8c8b0a27a1f36e10a8adcb30d9d175b0de1fbb
Forraz <forraz222@gmail.com>
2025-11-26 17:16:13 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import bodyParser from "body-parser";
+import express from "express"
+import dotenv from "dotenv"
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
+server.use(bodyParser.json());
+
+server.listen(PORT, () => {
+
+	console.log(`Listening to: ${PORT}`);
+
+});
